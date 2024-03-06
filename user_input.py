fa4e2bf7f134fbfcf130d95318c0cc9e863f0a1c
Faith-2004 <148619027+Faith-2004@users.noreply.github.com>
2024-03-06 02:06:01 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name=input("what is your name: ")
+print("name"+name)
+
+age=input("what is your age: ")
+print("age"+age)
+
+location=input("where are you located: ")
+print("location"+location)
+
+
+print("Hello  "+ name  +" you are  "+age  +" years old and live in  "+location)
